08b6c50197ed7ffca3d7d09e5a39e902e653de94
sethut1224 <sethut1224@gmail.com>
2024-02-06 11:12:57 +0900

    minor revision

--- include/file_io.hpp
+++ include/file_io.hpp
@@ -23,14 +23,12 @@ public:
     
     void open_input_stream(const fs::path& path, std::ios_base::openmode mode = std::ios_base::in);
     void open_input_stream(const char *path, std::ios_base::openmode mode = std::ios_base::in);
-
     void open_output_stream(const fs::path& path, std::ios_base::openmode mode = std::ios_base::out);
     void open_output_stream(const char *path, std::ios_base::openmode mode = std::ios_base::out);
 
     void close_input_stream();
     void close_output_stream();
     
-
     const bool access_same_file() const;
     const bool input_stream_ok() const;
     const bool output_stream_ok() const;
